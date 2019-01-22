3887c95d330d4f2c3bb29c646962d420d8f725c4
FZhu <hzhu225@gmail.com>
2019-01-22 15:51:49 +1100

    Add date picker

--- app/src/main/java/com/cms/dialogforalert/MainActivity.java
+++ app/src/main/java/com/cms/dialogforalert/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cms.dialogforalert;
 
 import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -41,5 +42,21 @@ public class MainActivity extends AppCompatActivity
                 });
 
         myAlertBuilder.show();
+    }
+
+    public void showDatePicker(View view)
+    {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.datepicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day)
+    {
+        String day_string = Integer.toString(day);
+        String month_string = Integer.toString(month+1);
+        String year_string = Integer.toString(year);
+        String dateMessage = (day_string + "/" + month_string + "/" + year_string);
+
+        Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
     }
 }
